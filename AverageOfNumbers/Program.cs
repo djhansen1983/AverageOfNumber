c4de572bef7a7653239df36f2aa38584d641d561
djhansen <david.j.hansen@gmail.com>
2020-06-18 11:26:31 -0400

    Add project files.

---
+++ AverageOfNumbers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+
+namespace AverageOfNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            {
+
+                for(var idx=1; idx<=30; idx++) //a 'for loop'
+                {
+                    if(idx%3==0 && idx%5==0)
+                        Console.WriteLine("FIZZBUZZ ");
+                    else if(idx%3==0 && idx%5!=0)
+                        Console.WriteLine("FIZZ ");
+                    else if(idx%3!=0 && idx%5==0)
+                        Console.WriteLine("BUZZ ");
+                    else
+                        Console.WriteLine($"{idx} ");
+
+
+                }
+
+
+            }
+
+           
+          
+            
+        }
+    }
+}
